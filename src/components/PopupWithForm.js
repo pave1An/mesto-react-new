0b0a243971caf52c598cd5d7ac982d7a7cb7750b
pave1An <116518087+pave1An@users.noreply.github.com>
2023-04-23 20:08:51 +0300

    Update PopupWithForm.js

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function PopupWithForm({ isOpen, onClose, formData: {name, title, buttonText}, children}) {
   return ( 
-	  <div className={`popup ${isOpen && 'popup_opened'} popup_type_${name}`}>
+    <div className={`popup ${isOpen && 'popup_opened'} popup_type_${name}`}>
       <div className= 'popup__container'>
         <button type="button" onClick={onClose} className="popup__close-btn" name="form-close" aria-label="Закрыть" />
         <h3 className="popup__title">{title}</h3>
